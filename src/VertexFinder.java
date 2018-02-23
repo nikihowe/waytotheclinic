4d9e632618c070af4c5b5f1bfbbacbfa2c9fa1e9
nikihowe <nkhrrghw@gmail.com>
2018-02-23 15:37:32 +0000

    Attempt multifloor

--- src/VertexFinder.java
+++ src/VertexFinder.java
@@ -15,8 +15,8 @@ import static java.lang.Math.min;
 
 public class VertexFinder {
 
-    private static List<BufferedImage> lineImages;
-    private static List<BufferedImage> mapImages;
+    private static List<BufferedImage> lineImages = new ArrayList<>();
+    private static List<BufferedImage> mapImages = new ArrayList<>();
     private static int width = 0;
     private static int height = 0;
 //    private static int level = 2;
@@ -24,15 +24,15 @@ public class VertexFinder {
     private static int LINE = 1;
     private static int MAP = 2;
 
-    /*private static JFrame showCropped(int x, int y, int cropWidth, int cropHeight, int mode) {
+    private static JFrame showCropped(int x, int y, int z, int cropWidth, int cropHeight, int mode) {
 
         BufferedImage image;
         int dotColour;
         if (mode == LINE) {
-            image = lineImage;
+            image = lineImages.get(z);
             dotColour = 0xFF00FF00;
         } else if (mode == MAP){
-            image = mapImage;
+            image = mapImages.get(z);
             dotColour = 0xFFFF0000;
         } else {
             return null;
@@ -60,7 +60,7 @@ public class VertexFinder {
         image.setRGB(x, y, tmpColor); // set colour back
 
         return frame;
-    }*/
+    }
 
     public static final BufferedImage deepCopy(BufferedImage image) {
 //        BufferedImage clone = UIUtil.createImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
@@ -88,7 +88,7 @@ public class VertexFinder {
         roomColour.put("black", "Hall");
         roomColour.put("white", "Wall");
 
-        List<HashSet<Vertex>> verticesByFloor;
+        HashSet<Vertex> vertexSet = new HashSet<>(); // will store all the vertices
 
         // Colour -> set of points
         Map<Integer, Set<Vertex>> stairMap = new HashMap<>();
@@ -96,8 +96,10 @@ public class VertexFinder {
         // level 0 == level 2; level 1 == level 3
         for (int level = 0; level <= 1; level++) { // note that until we have floors 0 and 1, we'll be offset
 
+
             // Get the the lines image (used for parsing in the map)
             // and the actual image (used for asking to label nodes)
+
             lineImages.add(javax.imageio.ImageIO.read(new File(lineLocation2)));
             mapImages.add(javax.imageio.ImageIO.read(new File(mapLocation2)));
 
@@ -108,7 +110,6 @@ public class VertexFinder {
             assert (height == mapImages.get(level).getHeight());
 
             // Will be used to store vertices and edges adjacent to each vertex
-            verticesByFloor = new ArrayList<>();
             HashMap<Pair<Integer, Integer>, Vertex> coordinateMap = new HashMap<>();
             HashMap<Vertex, HashSet<Edge>> adjList = new HashMap<>();
 
@@ -123,75 +124,101 @@ public class VertexFinder {
                     int col = lineImages.get(level).getRGB(i, j);
 //                System.out.println("Vertex: " + i + "," + j + " has colour " + RoomType.getColour(col));
 
-                    if (isFilled(i, j)) { // if the pixel isn't white
+                    if (isFilled(i, j, level)) { // if the pixel isn't white
                         // Get the pixel's colour (used to deduce type)
                         int colourRGB = lineImages.get(level).getRGB(i, j);
 
 //                    System.out.println("Vertex " + i + "," + j + " is " + RoomType.getColour(colourRGB));
 
+                        // Add a vertex
                         if (!RoomType.isBlack(colourRGB)) { // it is a vertex
                             Vertex v = new Vertex(i, j, level);
-                            verticesByFloor.get(level).add(v); // store in our set of vertices
+                            vertexSet.add(v); // store in our set of vertices
                             coordinateMap.put(new Pair(i, j), v);
                             adjList.put(v, new HashSet<>()); // make a new entry in our set of edges of this vertex
                             numVertices++;
+//                            System.out.println("vertex " + v + " has adjlist " + adjList.get(v));
+
+                            // Check if stairs
+                            if (RoomType.isYellow(colourRGB)) {
+                                if (stairMap.keySet().contains(colourRGB)) { // we already have a stair of this colour
+                                    stairMap.get(colourRGB).add(v);
+                                } else { // first instance of this colour, so make a new set
+                                    HashSet<Vertex> temp = new HashSet<>();
+                                    temp.add(v);
+                                    stairMap.put(colourRGB, temp);
+                                }
+                            }
                         }
                     }
                 }
             }
+        }
 
-//        for (Vertex v : vertexSet) {
-//            int col = lineImage.getRGB(v.getX(), v.getY());
-//            System.out.println("Vertex: " + v + " has colour " + RoomType.getColour(col));
-//        }
+        System.out.println("Number of Pixels: " + width * height);
+        System.out.println("Number of Vertices: " + numVertices);
 
-            // Have added all the vertices; now it's time to add the edges
-            int[][] dir = new int[][]{ {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
-            double[] angles = new double[] { 180, 90, 0, 270 };
+//            System.out.println(adjList);
+        System.out.println(adjList.size());
 
-            // Connect vertices on the same level (not stairs/lifts)
-            for (Vertex v : verticesByFloor.get(level)) {
-                int x = v.getX();
-                int y = v.getY();
+        // Have added all the vertices; now it's time to add the edges
+        // We start with automatic edge detection in the cardinal directions
+        int[][] dir = new int[][]{ {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
+        double[] angles = new double[] { 180, 90, 0, 270 };
 
-                // For each direction around the pixel, find the next edge to add
-                for (int b = 0; b < dir.length; b++) { // for each direction
-                    for (int i = 1; i < Integer.MAX_VALUE; i++) { // check until we hit a wall
+        for (Vertex v : vertexSet) {
+            int x = v.getX();
+            int y = v.getY();
+            int z = v.getZ();
 
-                        int checkX = x + dir[b][0] * i; // how far on x
-                        int checkY = y + dir[b][1] * i; // how far on y (it'll only be one of these)
+            // For each direction around the pixel, find the next edge to add
+            for (int b = 0; b < dir.length; b++) { // for each direction
+                for (int i = 1; i < Integer.MAX_VALUE; i++) { // check until we hit a wall
 
-                        int pixelColour = lineImages.get(level).getRGB(checkX, checkY);
-                        // We want to add an edge to the pixel at this location
-                        if (RoomType.notBW(pixelColour)) {
-                            Vertex w = coordinateMap.get(new Pair(checkX, checkY));
-                            assert (!v.samePlaceAs(w));
-                            adjList.get(v).add(new Edge(v, w, i, angles[b]));
+                    int checkX = x + dir[b][0] * i; // how far on x
+                    int checkY = y + dir[b][1] * i; // how far on y (it'll only be one of these)
+
+                    if (!isFilled(checkX, checkY, z)) {
+                        break;
+                    }
+                    int pixelColour = lineImages.get(z).getRGB(checkX, checkY);
+                    // We want to add an edge to the pixel at this location
+                    if (RoomType.notBW(pixelColour)) {
+                        Vertex w = coordinateMap.get(new Pair(checkX, checkY));
+//                            System.out.println(adjList.get(v));
+//                            Set<Edge> myList = adjList.get(v);
+//                            System.out.println(myList);
+                        System.out.println(v);
+                        adjList.get(v).add(new Edge(v, w, i, angles[b]));
 //                        System.out.println("Added: " + adjList.get(v));
-                            break; // only get the first adjacent node
-                        }
+                        break; // only get the first adjacent node
+                    }
 //                    JFrame frame = showCropped(checkX, checkY, 80, 80, LINE);
 //                    frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
 
-                    }
                 }
             }
 
-            // Connect vertices on different levels (stairs/lifts)
+            // Make edges between stair/lift vertices on different levels
+            int vertexColour = lineImages.get(z).getRGB(x, y);
+            // Stairs
+            if (RoomType.isYellow(vertexColour)) {
+
+
+            }
 
 
 
 
-            System.out.println("Number of Pixels: " + width * height);
-            System.out.println("Number of Vertices: " + numVertices);
 
+            // Labelling section
             System.out.println("Labelling... ");
             boolean autofill = true;
             int i = -1;
             for (Vertex v : vertexSet) {
                 i++;
-                if (!RoomType.isGrey(lineImage.getRGB(v.getX(), v.getY()))) {
-                    v.addLabel(roomColour.get(RoomType.getColour(lineImage.getRGB(v.getX(), v.getY()))));
+                if (!RoomType.isGrey(lineImages.get(v.getZ()).getRGB(v.getX(), v.getY()))) {
+                    v.addLabel(roomColour.get(RoomType.getColour(lineImages.get(v.getZ()).getRGB(v.getX(), v.getY()))));
                     continue; // go to the next node
                 }
 
@@ -200,7 +227,7 @@ public class VertexFinder {
                     continue;
                 }
 
-                JFrame frame = showCropped(v.getX(), v.getY(), 200, 200, MAP);
+                JFrame frame = showCropped(v.getX(), v.getY(), v.getZ(), 200, 200, MAP);
                 System.out.println("Add labels, seperated by commas: ");
 
                 String[] line = stdin.nextLine().split(",");
@@ -219,7 +246,7 @@ public class VertexFinder {
                 frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
             }
 
-            printResult(vertexSet, adjList);
+//            printResult(vertexSet, adjList);
 
             System.out.println("Saving results");
 
@@ -238,10 +265,13 @@ public class VertexFinder {
             oos2.flush();
             oos3.flush();
 
+//            for (Integer j : stairMap.keySet()) {
+//                System.out.println("stairs: " + stairMap.get(j));
+//            }
+
             System.out.println("Done");
         }
     }
-
 
     private static void printResult(HashSet<Vertex> vertexSet, HashMap<Vertex, HashSet<Edge>> adjList) {
         System.out.println("Vertices: ");
@@ -265,8 +295,13 @@ public class VertexFinder {
         }
     }
 
-    public static boolean isFilled(int x, int y) {
+    public static boolean isFilled(int x, int y, int z) {
         if (x < 0 || x >= width || y < 0 || y >= height) return false;
-        return !RoomType.isWhite(lineImage.getRGB(x, y));
+        return !RoomType.isWhite(lineImages.get(z).getRGB(x, y));
     }
+
+    public static boolean outOfBounds(int x, int y) {
+        return (x < 0 || x >= width || y < 0 || y >= height);
+    }
+
 }
